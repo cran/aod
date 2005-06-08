a5e8eb6327aa09419f47cf9bc16fdf7ea05fcdd7
Renaud Lancelot <renaud.lancelot@cirad.fr>
2005-06-08 00:00:00 +0000

    version 1.1-2

--- R/show-glimML.R
+++ R/show-glimML.R
@@ -1,7 +1,7 @@
 if(!isGeneric("summary"))
   setGeneric(name = "summary", def = function(object, ...) standardGeneric("summary"))
 
-### summary method for glimML objects
+## summary method for glimML objects
 setMethod("summary", signature = "glimML", 
   function(object){
 # function to insert NAs in se when there are NAs in b
@@ -19,24 +19,30 @@ setMethod("summary", signature = "glimML",
         }
       se
       }
+
 # check whether any fixed-effect coef was set to a fixed value
     nb <- length(coef(object))
     param <- object@param
     vpar <- object@varparam
+
 # position of fixed-effect coefficients
     pos1 <- seq(nb)
+
 # position of parameters set to a fixed value, if any
     fp <- match("fixpar", table = names(object@CALL))
     pos2 <- NA
     if(!is.na(fp))
       pos2 <- eval(object@CALL$fixpar[[2]])
+
 # remove fixed parameters, if any
     pos3 <- setdiff(pos1, pos2)
     Coef <- data.frame()
+
 # compute new var-cov mat, coef vector and position of term(s) to be tested
     if(length(pos3) > 0){
       vp3 <- as.matrix(vpar[pos3, pos3])
       b3 <- param[pos3]
+
 # coef, se, z and t test
       se3 <- sqrt(diag(vp3))
       se3 <- insNA(b3, se3)
@@ -57,22 +63,27 @@ setMethod("summary", signature = "glimML",
     nb <- length(coef(object))
     param <- object@param
     vpar <- object@varparam
+
 # position of overdispersion parameters
     pos1 <- (nb + 1):length(param)
+
 # position of parameters set to a fixed value, if any
     fp <- match("fixpar", table = names(object@CALL))
     pos2 <- NA
     if(!is.na(fp))
       pos2 <- eval(object@CALL$fixpar[[2]])
+
 # remove fixed parameters, if any
     pos3 <- setdiff(pos1, pos2)
     Phi <- data.frame()
+
 # compute new var-cov mat, coef vector and position of term(s) to be tested
     if(length(pos3) > 0){
       vp3 <- as.matrix(vpar[pos3, pos3])
       b3 <- param[pos3]
       se3 <- sqrt(diag(vp3))
       se3 <- insNA(b3, se3)
+
 # coef, se, z and t test for phi
 # beware: unilateral test for phi because it cannot be negative
       if(any(b3 < 0))
@@ -82,11 +93,11 @@ setMethod("summary", signature = "glimML",
       rownames(Phi) <- nam
       colnames(Phi) <- c("Estimate", "Std. Error", "z value", "Pr(> z)")
       }
+
 # print random coefficients which were set to a fixed value, if any
     pos4 <- setdiff(pos1, pos3)
     FixedPhi <- data.frame()
     if(length(pos4) > 0){
-      cat("\nOverdispersion coefficients set to fixed values:\n")
       FixedPhi <- data.frame(Value = param[pos4])
       }
     res <- new(Class = "summary.glimML",
@@ -94,10 +105,12 @@ setMethod("summary", signature = "glimML",
     res
     })
 
+
 ### show method for objects of class summary.glimML
 setMethod("show", signature = "summary.glimML", 
   function(object){
   Object <- object@object
+
 # Title and call
     switch(Object@method,
       BB = cat("Beta-binomial model\n", "-------------------\n", sep = ""),
@@ -130,6 +143,7 @@ setMethod("show", signature = "summary.glimML",
       cat("\nFixed-effect coefficients:\n")
       print(Coeftext)
       }
+
 # print fixed-effect coefficients which were set to a fixed value, if any
     FixedCoef <- object@FixedCoef
     if(nrow(FixedCoef) > 0){
@@ -138,8 +152,8 @@ setMethod("show", signature = "summary.glimML",
       }
 
 # overdispersion coefficients phi
-    Phi <- object@Phi
 # compute new var-cov mat, coef vector and position of term(s) to be tested
+    Phi <- object@Phi
     if(nrow(Phi) > 0){
       nam <- rownames(Phi)
       List <- vector(mode = "list", length = 4)
@@ -156,6 +170,7 @@ setMethod("show", signature = "summary.glimML",
       cat("\nOverdispersion coefficients:\n")
       print(Phitext)
       }
+
 # print overdispersion coefficients which were set to a fixed value, if any
     FixedPhi <- object@FixedPhi
     if(nrow(FixedPhi) > 0){
@@ -167,8 +182,9 @@ setMethod("show", signature = "summary.glimML",
     cat("nbpar = ", Object@nbpar, "; ", sep = "")
     cat("df.residual = ", df.residual(Object), "; ", sep = "")
     cat("Deviance = ", format(round(deviance(Object), 3), nsmall = 3), "; ", sep = "")
-    cat("AIC = ", format(round(-2 * Object@logL + 2 * Object@nbpar, 3), nsmall = 3), "\n\n", sep = "")
+    cat("AIC = ", format(round(AIC(Object), 3), nsmall = 3), "\n\n", sep = "")
     invisible(object)
+
     })
 
 
